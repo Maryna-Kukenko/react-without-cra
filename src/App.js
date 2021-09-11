aec3ee82a1876f26575b30462692066bdb0305d1
Maryna Kukenko <maryna.kukenko@inoxoft.com>
2021-09-11 22:50:06 +0300

    made some configuration changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">Hello World</div>
+  );
+};
+
+export default hot(module)(App);
